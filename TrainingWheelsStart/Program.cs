972cc41f0426aa425c73e2dbaddf5e6d5ecdfeba
MD <m.diederen@adelante-zorggroep.nl>
2019-07-23 09:30:57 +0200

    First Program

--- TrainingWheelsStart/Program.cs
+++ TrainingWheelsStart/Program.cs
@@ -1,21 +1,34 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TrainingWheelsStart
+namespace TrainingWheelsStart  // ==> This is the namespace
 {
-    class Program
+    class Program  // ==> this is your class declaration
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// These are your first lines of code, for now this is all you need to now
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) // ==> This is your method declaration (starts automatically when hittin F5)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            FirstProgram();  // ==> this is a call to a other Method (we run the function on a other location)
 
+            // the console will only be closed when a key (on the keyboard) is hit
+            // If we do not use this statement, we will not see the solution (windows opens en closes again)
+            Console.ReadKey();
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        } // ==> end first method
+
+
+        /// <summary>
+        /// THis is your first program
+        /// this is also called a method and for now it is only calculating  24 x 17
+        /// Then it will display the solution
+        /// </summary>
+        public static void FirstProgram() 
+        {
+            int MathSolution = 24 * 17;
+
+            Console.WriteLine("Math solution : " + MathSolution);
         }
     }
 }
